4cf08d3da507141155188635d5c1ec9b1731e2bf
TetianaKuzma <tetiana.b97@gmail.com>
2022-12-02 12:09:59 +0200

    Update color switcher by adding class

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,24 +1,49 @@
-const startBtn = document.querySelector('button[data-start]');
-const stopBtn = document.querySelector('button[data-stop]');
-let intervalId = null;
-stopBtn.disabled = true;
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+
+class ColorSwitch {
+  constructor() {
+    this.intervalId = null;
+    refs.stopBtn.disabled = true;
+  }
+
+  start() {
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    this.intervalId = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+
+  stop() {
+    clearInterval(this.intervalId);
+
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+  }
+}
+
+// Get random bg color
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startBtn.addEventListener('click', () => {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
+// Create instance of ColorSwitch
 
-  intervalId = setInterval(() => {
-    document.body.style.backgroundColor = getRandomHexColor();
-  }, 1000);
+const switcher = new ColorSwitch();
+
+// Start color switching after clicking 'Start' button
+
+refs.startBtn.addEventListener('click', () => {
+  switcher.start();
 });
 
-stopBtn.addEventListener('click', () => {
-  clearInterval(intervalId);
+// Stop color switching after clicking 'Stop' button
 
-  startBtn.disabled = false;
-  stopBtn.disabled = true;
+refs.stopBtn.addEventListener('click', () => {
+  switcher.stop();
 });
